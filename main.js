05a6ab517d3fe66d1073b3376955a88c715876dc
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-14 15:39:04 +0200

    add firt exercise - triangle loop

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+
+
+
+let piece = '';
+for (let i = 0; i <= 7; i++) {
+    piece += '#';
+    console.log(piece);
+}
